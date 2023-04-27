b4070c211f808838ebd42cf8884bfd0cf68fc8dd
moritzpfefferkorn@hotmail.com <moritzpfefferkorn@hotmail.com>
2023-04-27 21:36:32 +0200

    Update checkThreaded.h

--- checkThreaded.h
+++ checkThreaded.h
@@ -2,10 +2,10 @@
 #define CHECKTHREADED_H
 
 #include <vector>
-#include <mutex>
-#include <condition_variable>
 
-void checkPrimeThreaded(std::vector<int>& primes, std::vector<int>& output, std::mutex& outputMutex, std::vector<int>& status, const int threadNumber, const int threadCount, const int max);
+#include "checkComs.h"
+
+void checkPrimeThreaded(std::vector<int>& primes, CheckComs& communications);
 
 #ifdef __AVX__
 
