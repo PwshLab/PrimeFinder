08200b08f10d11c97d38a6f5ba4d1f661f36ec2d
moritzpfefferkorn@hotmail.com <moritzpfefferkorn@hotmail.com>
2023-04-27 21:33:40 +0200

    Create numberPair.h

---
+++ numberPair.h
@@ -0,0 +1,14 @@
+#ifndef NUMBERPAIR_H
+#define NUMBERPAIR_H
+
+struct NumberPair
+{
+    int int1;
+    int int2;
+    NumberPair(int i1, int i2) {
+        int1 = i1;
+        int2 = i2;
+    }
+};
+
+#endif
