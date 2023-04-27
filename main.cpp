f023dcde7392a312198f0b60756fb28093beb67a
moritzpfefferkorn@hotmail.com <moritzpfefferkorn@hotmail.com>
2023-04-27 21:36:33 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include "numberPair.h"
 #include "checkPrime.h"
 #include "checkThreaded.h"
+#include "checkComs.h"
 
 using namespace std;
 
@@ -16,12 +17,10 @@ int main()
 {   
     auto started = chrono::high_resolution_clock::now();
 
-    int max = 10000000; // für höhere werte wird der double type benötigt (nicht implementiert bei avx wegen start verringerter leistung)
+    const int max = 10000000; // für höhere werte wird der double type benötigt (nicht implementiert bei avx wegen start verringerter leistung)
     int concurrency = thread::hardware_concurrency();
-    concurrency = 4;
 
     vector<int> primes = {2};
-    // vector<int> primes = {2, 3, 5, 7, 11};
     #ifdef __AVX__
     vector<float> inversePrimes = {1.0f/2};
     #endif
@@ -54,30 +53,17 @@ int main()
     }
     else
     {
-        // lock funktioniert nicht so wie gedacht. rip. braucht nen kompletten rewrite.
-        // arbeite dran.
         vector<thread*> threads;
         threads.reserve(concurrency);
-        vector<int> status(concurrency);
-        vector<vector<int>> outputs(concurrency);
-        vector<mutex> mutexOutputs(concurrency);
-
-        // Thread Status Table
-        // 1: Working
-        // 2: Completed
-        // 3: Queued for Waiting
-        // 4: Waiting
-
-        // cout << "Starting Threads" << endl;
+        vector<CheckComs> coms(concurrency);
 
         for (size_t i = 0; i < concurrency; i++)
         {
             int threadNumber = i;
-            status[i] = 1;
             #ifdef __AVX__
             threads[i] = thread::thread(checkPrimeThreaded, primes, ref(inputs[i]), ref(outputs[i]), status, threadNumber); //fix avx function (inverse primes parameter)
             #else
-            thread* t = new thread(checkPrimeThreaded, ref(primes), ref(outputs[i]), ref(mutexOutputs[i]), ref(status), threadNumber, concurrency, max);
+            thread* t = new thread(checkPrimeThreaded, ref(primes), ref(coms[threadNumber]));
             threads.push_back(t);
             #endif
         }
@@ -92,179 +78,61 @@ int main()
         //     largestPrimes[i] = INT_MAX;
         // }
         
+        const int increment = 2;
+        int minNumbersPerThread = concurrency * concurrency;
+        int currentNumber = 3;
 
-        bool completed = false;
-        while (!completed)
-        {   
-            // cout << "Checking if completed" << endl;
-            completed = true;
-            for (size_t i = 0; i < status.size(); i++)
+        while (currentNumber < max)
+        {
+            int currentMax = min(primes.back() * primes.back() - 1, max);
+            int nextSize = currentMax - currentNumber;
+            int offset = nextSize % concurrency;
+            int nextBlockSize = (nextSize - offset) / concurrency;
+            if (nextBlockSize < minNumbersPerThread)
             {
-                if (status[i] < max)
+                for (; currentNumber < currentMax; currentNumber += increment)
                 {
-                    completed = false;
-                    break;
+                    if (checkPrimeF(primes, currentNumber))
+                    {
+                        primes.push_back(currentNumber);
+                    }
                 }
             }
-            
-            // zahlen aus den outputs in den primzahl vektor geordnen einfügen
-            // cout << "Reading Outputs" << endl;
-            for (size_t i = 0; i < concurrency; i++)
+            else
             {
-                // cout << "Checking Output" << endl;
-                if (!outputs[i].empty())
+                for (size_t i = 0; i < concurrency; i++)
                 {
-                    // cout << "Locking Output" << endl;
-                    lock_guard<mutex> lock(mutexOutputs[i]);
-                    // cout << "Expanding Result" << endl;
-                    result.reserve(result.size() + outputs[i].size());
-                    // cout << "Reading Output" << endl;
-                    result.insert(result.end(), outputs[i].begin(), outputs[i].end());
-                    // cout << "Clearing Output" << endl;
-                    outputs[i].clear();
-                    // largestPrimes[i] = results[i][results[i].size() - 1];
+                    int lastNumber = currentNumber + nextBlockSize;
+                    coms[i].AddJob(CheckJob(currentNumber, lastNumber, increment));
+                    currentNumber = lastNumber;
                 }
-                // if (!results[i].empty())
-                // {
-                //     // cout << "Logging Largest Prime" << endl;
-                //     largestPrimes[i] = results[i][results[i].size() - 1];
-                //     // cout << "Saved Largest Prime" << endl;
-                // }
-                
-            }
 
-            // cout << "Finding Smallest Element" << endl;
-            // int smallestLargestIndex = distance(la.begin(), min_element(largestPrimes.begin(), largestPrimes.end()));
-            // int smallestLargestPrime = largestPrimes[smallestLargestIndex];
-            // int smallestSearched = status[distance(status.begin(), min_element(status.begin(), status.end()))];
-            int smallestSearched = INT_MAX;
-            for (size_t i = 0; i < status.size(); i++)
-            {
-                int value = status[i];
-                if (value < smallestSearched)
+                bool completed = false;
+                while (!completed)
                 {
-                    smallestSearched = value;
+                    completed = true;
+                    for (size_t i = 0; i < concurrency; i++)
+                    {
+                        if (coms[i].IsWorking())
+                        {
+                            completed = false;
+                            break;
+                        }
+                    }
                 }
-            }
-            // if (smallestSearched == INT_MAX)
-            // {
-            //     smallestSearched = -1;
-            // }
-            
 
-            // cout << "smalles prime " << smallestLargestPrime << endl;
-            
-            // cout << "Splicing Vectors" << endl;
-            // for (size_t i = 0; i < concurrency; i++)
-            // {   
-            //     if (!results[i].empty())
-            //     {
-            //         vector<int>::iterator lastElement = upper_bound(results[i].begin(), results[i].end(), smallestLargestPrime);
-            //         if (lastElement <= results[i].begin())
-            //             continue;
-            //         result.insert(result.end(), results[i].begin(), lastElement);
-            //         results[i].erase(results[i].begin(), lastElement);
-            //     }
-            // }
-            
-            // cout << "Sorting Result" << endl;
-            
-
-            if (!result.empty())
-            {
-                // vector<int> newPrimes (primes.size() + result.size());
-                // #ifdef __AVX__
-                // vector<float> newInversePrimes (newPrimes.size());
-                // #endif
-
+                vector<int> result;
+                for (size_t i = 0; i < concurrency; i++)
+                {
+                    if (coms[i].HasResult())
+                    {
+                        coms[i].ReceiveResultUnsorted(result);
+                    }
+                }
                 sort(result.begin(), result.end());
-                
-                // merge(primes.begin(), primes.end(), result.begin(), result.end(), newPrimes.begin());
-                // swap(primes, newPrimes);
-                // result.clear();
-                vector<int>::iterator lastElement = upper_bound(result.begin(), result.end(), smallestSearched);
-                if (lastElement == result.begin())
-                {
-                    continue;
-                }
-                vector<int> newPrimes(primes.size() + distance(result.begin(), lastElement));
-                // primes.insert(primes.end(), result.begin(), lastElement);
-                merge(primes.begin(), primes.end(), result.begin(), lastElement, newPrimes.begin());
-                result.erase(result.begin(), lastElement);
 
-                // for (size_t i = 0; i < concurrency; i++)
-                // {
-                //     status[i] = 3;
-                // }
-
-                // bool waiting;
-                // do
-                // {
-                //     waiting = true;
-                //     for (size_t i = 0; i < concurrency; i++)
-                //     {
-                //         if (status[i] != 4)
-                //         {
-                //             waiting = false;
-                //             break;
-                //         }
-                //     }
-                    
-                // } while (!waiting);
-                
-                swap(primes, newPrimes);
-
-                // for (size_t i = 0; i < concurrency; i++)
-                // {
-                //     status[i] = 1;
-                // }
-
-                // #ifdef __AVX__
-                // for (size_t i = 0; i < primes.size(); i++)
-                // {
-                //     newInversePrimes.push_back(1/primes[i]);
-                // }
-                // swap(inversePrimes, newInversePrimes);
-                // #endif
+                primes.insert(primes.end(), result.begin(), result.end());
             }
-            
-            // for (size_t j = 0; j < outputs.size(); j++)
-            // {   unique_lock lock(mutexOutputs[j]);
-            //     while (!outputs[j].empty())
-            //     {
-            //         int number = outputs[j].front();
-            //         outputs[j].pop();
-            //         results.push_back(number);
-            //     }
-            //     lock.unlock();
-            // }
-
-            // sort(results.begin(), results.end());
-            // #ifdef __AVX__
-            // vector<int> inverseResults(results.size());
-            // for (size_t j = 0; j < results.size(); j++)
-            // {
-            //     inverseResults.push_back(1.0f/results[j]);
-            // }
-            // #endif
-
-            // unique_lock lock1(primes);
-            // #ifdef __AVX__
-            // unique_lock lock2(inversePrimes);
-            // #endif
-
-            // cout << "Adding Primes" << endl;
-            // primes.reserve(primes.size() + result.size());
-            // primes.insert(primes.end(), result.begin(), result.end());
-            // #ifdef __AVX__
-            // inversePrimes.reserve(inversePrimes.size() + inverseResults.size());
-            // inversePrimes.insert(inversePrimes.end(), inverseResults.begin(), inverseResults.end());
-            // #endif
-            
-            // cout << "Cleanup and update" << endl;
-                // cout << "notifying all" << endl;
-                // sync.notify_all();
-            // cout << "Done" << endl;
         }
         
         // unique_lock lock(mutexStatus);
