4e886d1f62171f8039282537cc8c1a9235fc1220
moritzpfefferkorn@hotmail.com <moritzpfefferkorn@hotmail.com>
2024-06-15 00:43:48 +0200

    Update checkPrime.h

--- checkPrime.h
+++ checkPrime.h
@@ -3,8 +3,8 @@
 
 #include <vector>
 
-bool checkPrime(vector<uint32_t>& primes, vector<uint32_t>& primesInverse, uint32_t number);
+bool checkPrime(vector<uint64_t>& primes, vector<uint64_t>& inversePrimes, uint32_t number);
 
-bool checkPrimeAVX(std::vector<uint32_t>& primes, std::vector<uint32_t>& primesInverse, uint32_t number);
+bool checkPrimeAVX(std::vector<uint64_t>& primes, std::vector<uint64_t>& inversePrimes, uint32_t number);
 
 #endif
