3aedaf375575021cbaa8ecbdb476298b81f532a3
moritzpfefferkorn@hotmail.com <moritzpfefferkorn@hotmail.com>
2024-06-15 00:43:49 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,20 +17,20 @@ int main()
     auto started = chrono::high_resolution_clock::now();
 
     const uint32_t max = pow(2, 24); // 16777216
-    vector<uint32_t> primes = {2};
-    vector<uint32_t> primesInverse = {getReciprocal(2)};
+    vector<uint64_t> primes = {2};
+    vector<uint64_t> inversePrimes = {getReciprocal(2)};
 
     // https://de.wikipedia.org/wiki/Primzahlsatz
     const int estimate = ceilf( ((float)max / log(max)) * 1.1f ); // 10% korrektur nach oben zur sicherheit (spart 1ms bei ausfürung)
     primes.reserve(estimate);
-    primesInverse.reserve(estimate);
+    inversePrimes.reserve(estimate);
     
     for (size_t i = 3; i < max; i += 2)
     {   
-        if (checkPrimeAVX(primes, primesInverse, i))
+        if (checkPrimeAVX(primes, inversePrimes, i))
         {   
             primes.push_back(i);
-            primesInverse.push_back(getReciprocal(i));
+            inversePrimes.push_back(getReciprocal(i));
         }
     }
     
